679a846f6d8b078331ab288d2f626f423c4e17b8
RavinduFernando <f.ravindu@gmail.com>
2020-05-27 20:14:21 +0530

    added the api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 
-
+import { Cards, Chart } from './components';
+import styles from './App.module.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
       </div>
     );
   }
---
+++ src/api/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const URL = "https://www.hpb.health.gov.lk/api/get-current-statistical";
+
