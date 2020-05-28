b866b853eee15f0f0a3c5b17aa997cc60edf028e
RavinduFernando <f.ravindu@gmail.com>
2020-05-28 13:57:43 +0530

    added 3 cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,92 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Cards = () => {
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
+
+export default function ImgMediaCard() {
+    const classes = useStyles();
 
     return (
-        <h1>Cards</h1>
-    )
+        <div>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            COVID-19
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Showing the current situation in COVID-19 in Sri lanka.
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+        </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
 
+
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            COVID-19
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Showing the current situation in COVID-19 in Sri lanka.
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+        </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+
+
+
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            COVID-19
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Showing the current situation in COVID-19 in Sri lanka.
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+        </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+        </div>
+
+    );
 }
-
-export default Cards;
-
