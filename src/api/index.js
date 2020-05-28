7e9238b7fe8b7329b72c049059ada31f06b4b876
RavinduFernando <f.ravindu@gmail.com>
2020-05-28 09:15:27 +0530

    fetched the data

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,41 @@ import axios from 'axios';
 
 const URL = "https://www.hpb.health.gov.lk/api/get-current-statistical";
 
+export const fetchData = async () => {
+
+    try {
+
+        const response = await axios.get(URL);
+
+        return response;
+
+    } catch (error) {
+
+    }
+
+
+}
+
+
+// daily_pcr_testing_data, global_deaths, global_new_cases, global_new_deaths, global_recovered, global_total_cases, hospital_data, local_active_cases, local_deaths, local_new_cases, local_new_deaths, local_recovered, local_total_cases, local_total_number_of_individuals_in_hospitals, total_pcr_testing_count, update_date_time
+
+// const modifiedData = {
+//     daily_pcr_testing_data,
+//     global_deaths,
+//     global_new_cases,
+//     global_new_deaths,
+//     global_recovered,
+//     global_total_cases,
+//     hospital_data,
+//     local_active_cases,
+//     local_deaths,
+//     local_new_cases,
+//     local_new_deaths,
+//     local_recovered,
+//     local_total_cases,
+//     local_total_number_of_individuals_in_hospitals,
+//     total_pcr_testing_count,
+//     update_date_time
+// }
+
+// return modifiedData;
