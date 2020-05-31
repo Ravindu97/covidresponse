46c4708a6c13e31f192d3d8a2476678fede54997
RavinduFernando <f.ravindu@gmail.com>
2020-05-31 12:50:50 +0530

    destructured the api to only get the data we need

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Cards, Chart } from './components/';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const { data: { data } } = await fetchData();
+
+    console.log(data);
+
+  }
+
+
 
   render() {
 
     return (
 
-      <div>
+      <div className={styles.container}>
         <h1>App</h1>
         <Cards />
         <Chart />
