cf90d1a17e035d46d65843f0286160626f4633e8
RavinduFernando <f.ravindu@gmail.com>
2020-05-31 13:18:44 +0530

    data passed into funtional components as props is not read .... Bug :(

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import { fetchData } from './api';
 
 class App extends React.Component {
 
-  async componentDidMount() {
-    const data = await fetchData();
 
-    console.log(data);
+  state = {
+    data: {},
+  }
+
+
+
+
+
+  async componentDidMount() {
+
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
 
   }
 
@@ -16,11 +26,14 @@ class App extends React.Component {
 
   render() {
 
+    // uesd to destructure data from the state
+    const { data } = this.state.data;
+
     return (
 
       <div className={styles.container}>
         <h1>App</h1>
-        <Cards />
+        <Cards data={data} />
         <Chart />
       </div>
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Cards = () => {
+const Cards = (props) => {
+
+    console.log(props);
+
 
     return (
         <h1>Cards</h1>
@@ -14,106 +17,3 @@ export default Cards;
 
 
 
-// import React from 'react';
-// // import styles from './Cards.modules.css';
-// import { makeStyles } from '@material-ui/core/styles';
-
-// import { Card, CardActionArea, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-
-
-// const Cards = (props) => {
-//     console.log(props);
-
-//     return (
-//         <div></div>
-//     )
-
-
-
-// }
-
-// export default Cards;
-
-
-// const useStyles = makeStyles({
-//     root: {
-//         maxWidth: 345,
-//     },
-// });
-
-// export default function ImgMediaCard() {
-//     const classes = useStyles();
-
-//     return (
-//         <div>
-//             <Card className={classes.root}>
-//                 <CardActionArea>
-//                     <CardContent>
-//                         <Typography gutterBottom variant="h5" component="h2">
-//                             COVID-19
-//           </Typography>
-//                         <Typography variant="body2" color="textSecondary" component="p">
-//                             Showing the current situation in COVID-19 in Sri lanka.
-//           </Typography>
-//                     </CardContent>
-//                 </CardActionArea>
-//                 <CardActions>
-//                     <Button size="small" color="primary">
-//                         Share
-//         </Button>
-//                     <Button size="small" color="primary">
-//                         Learn More
-//         </Button>
-//                 </CardActions>
-//             </Card>
-
-
-
-//             <Card className={classes.root}>
-//                 <CardActionArea>
-//                     <CardContent>
-//                         <Typography gutterBottom variant="h5" component="h2">
-//                             COVID-19
-//           </Typography>
-//                         <Typography variant="body2" color="textSecondary" component="p">
-//                             Showing the current situation in COVID-19 in Sri lanka.
-//           </Typography>
-//                     </CardContent>
-//                 </CardActionArea>
-//                 <CardActions>
-//                     <Button size="small" color="primary">
-//                         Share
-//         </Button>
-//                     <Button size="small" color="primary">
-//                         Learn More
-//         </Button>
-//                 </CardActions>
-//             </Card>
-
-
-
-
-//             <Card className={classes.root}>
-//                 <CardActionArea>
-//                     <CardContent>
-//                         <Typography gutterBottom variant="h5" component="h2">
-//                             COVID-19
-//           </Typography>
-//                         <Typography variant="body2" color="textSecondary" component="p">
-//                             Showing the current situation in COVID-19 in Sri lanka.
-//           </Typography>
-//                     </CardContent>
-//                 </CardActionArea>
-//                 <CardActions>
-//                     <Button size="small" color="primary">
-//                         Share
-//         </Button>
-//                     <Button size="small" color="primary">
-//                         Learn More
-//         </Button>
-//                 </CardActions>
-//             </Card>
-//         </div>
-
-//     );
-// }
