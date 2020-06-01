ffda4d16dcb9c1a7057b5b09f44ba6af76d60a5f
RavinduFernando <f.ravindu@gmail.com>
2020-06-01 10:52:07 +0530

    added a chart but the values are not passing

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,20 +1,130 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
-const Charts = (props) => {
-
-    console.log(props);
+class Charts extends Component {
 
 
-    return (
+    state = {
+        chartData: {}
+    }
 
-        <h1>Charts</h1>
+    componentWillMount() {
+        this.getChartData();
+    }
 
 
-    )
+
+
+    getChartData() {
+        this.setState({
+
+            chartData: {
+                labels: ['Active Cases', 'recovered', 'Deaths'],
+                datasets: [
+                    {
+                        label: 'People',
+                        data: [{ 25}, 35, 40],
+                        backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)']
+                    }
+                ]
+            }
+
+
+        })
+    }
+
+
+
+    render() {
+
+
+        const { data: { local_active_cases, local_recovered, local_deaths } } = this.props;
+
+
+
+
+
+
+
+
+        return (
+
+            <div>
+
+
+
+                <Bar
+
+                    data={this.state.chartData}
+                    options={{}}
+
+
+                />
+
+            </div>
+
+
+
+        )
+
+    }
+
 
 }
 
 export default Charts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dataSet = {
+//     chartData: {
+//         labels: ['Active Cases', 'recovered', 'Deaths'],
+//         datasets: [
+//             {
+//                 label: 'People',
+//                 data: [25, 35, 40],
+//                 backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)']
+//             }
+//         ]
+//     }
+// }
+
+
+
+// const Chart = () => {
+
+
+//     return (
+//         <div>
+
+//             <Bar
+
+//                 data={dataSet}
+//                 options={{}}
+
+
+//             />
+
+//         </div>
+//     )
+
+
+// }
+// export default Chart;
+
+
 
 
